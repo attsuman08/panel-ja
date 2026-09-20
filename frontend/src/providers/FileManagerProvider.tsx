fe8ff3e70e86981f69591bf0b989b986d9826430
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2026-09-20 12:18:58 +0200

    Refetch data on remount and window focus

--- frontend/src/providers/FileManagerProvider.tsx
+++ frontend/src/providers/FileManagerProvider.tsx
@@ -64,7 +64,6 @@ const FileManagerProvider = ({ children }: { children: ReactNode }) => {
     },
     enabled: canReadFiles,
     staleTime: 30_000,
-    refetchOnMount: 'always',
     placeholderData: keepPreviousData,
   });
 
